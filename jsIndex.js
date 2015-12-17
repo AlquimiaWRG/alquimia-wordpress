9d5d3a3130735d73cae363927cf89291de4535ed
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2015-12-17 16:50:14 +0100

    Fixed another issue thrown when alquimia-alquimia is not installed

--- jsIndex.js
+++ jsIndex.js
@@ -4,13 +4,21 @@ module.exports = function(defaultJs) {
   var alquimiaConfig = defaultJs.getElement('alquimia')
   if (alquimiaConfig) alquimiaConfig.wp = true;
 
-  defaultJs.getElement('configs').push(
-    "module.constant('SERVER', 'http://localhost/" + alquimia.config.appName.dashed + "/admin/');",
-    "module.config(['WPApiProvider', 'SERVER', function(WPApiProvider, SERVER) {",
-    "  WPApiProvider.setBaseUrl(SERVER + 'wp-json');",
-    "}]);",
-    ""
+  var configs = defaultJs.getElement('configs');
+
+  configs.push(
+    "module.constant('SERVER', 'http://localhost/" + alquimia.config.appName.dashed + "/admin/');"
   );
+
+  if (alquimia.config.packages.indexOf('alquimia') >= 0) {
+    configs.push(
+      "module.config(['WPApiProvider', 'SERVER', function(WPApiProvider, SERVER) {",
+      "  WPApiProvider.setBaseUrl(SERVER + 'wp-json');",
+      "}]);"
+    );
+  }
+
+  configs.push("");
 
   return defaultJs;
 };
